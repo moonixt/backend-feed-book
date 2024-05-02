1cef2d0d8861771ea0d3242191368be45519227a
Derek <99456748+moonixt@users.noreply.github.com>
2024-05-02 14:39:13 -0300

    added post details

--- backend_spot_album/urls.py
+++ backend_spot_album/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from spot_album.views import AlbumViewSet  #xxxx nome da pasta app
+from spot_album.views import AlbumViewSet, get_items_post #xxxx nome da pasta app
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,6 +13,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('albums-post/<int:id_post>/', get_items_post, name='albums-post'),
+ ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- spot_album/views.py
+++ spot_album/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Album
 from .serializers import AlbumSerializer
 from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 
@@ -10,3 +13,13 @@ from rest_framework.permissions import IsAuthenticated
 class AlbumViewSet(viewsets.ModelViewSet):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+
+
+@api_view(['GET'])
+def get_items_post(request, id_post):
+    
+    albums = Album.objects.filter(id = id_post)
+
+    album_s = AlbumSerializer(instance=albums ,many=True)
+
+    return Response(album_s.data, status=status.HTTP_200_OK)    
